b0bace55fc0749d6787c490332b8f07528cc42c4
Murat Guzel <muratgguzel77@yahoo.com>
2024-07-05 19:00:14 -0500

    7/5 Catalog.API push

---
+++ src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace Catalog.Api.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name,List <string> Category,string Description,string ImageFile,decimal Price);
+
+
+    public class CreateProductHandler
+    {
+    }
+}
