44de94e52bedbab33de477adb45b25f21ff4dd0a
Murat Guzel <muratgguzel77@yahoo.com>
2024-11-20 16:09:42 -0600

    q

--- src/BuildingBlocks/CQRS/IQueryHandler.cs
+++ src/BuildingBlocks/CQRS/IQueryHandler.cs
@@ -12,5 +12,5 @@ namespace BuildingBlocks.CQRS;
 public interface IQueryHandler<in TQuery, TResponse> : IRequestHandler<TQuery, TResponse> where TQuery : IQuery<TResponse> where TResponse : notnull
 {
 
-
+    //
 }
